9744e243e83edcf9758a9bf36f10088862bfc462
Hermes Lieberman <hermes.lieberman@gmail.com>
2026-06-28 12:57:08 -0400

    fix: use #[cfg] block for updater plugin instead of inline attribute (invalid in method chains)

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -405,13 +405,16 @@ pub fn run() {
         }));
     }
 
+    #[cfg(feature = "updater")]
+    {
+        builder = builder.plugin(tauri_plugin_updater::Builder::new().build());
+    }
+
     builder
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_dialog::init())
-        #[cfg(feature = "updater")]
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .manage(whisper_engine::parallel_commands::ParallelProcessorState::new())
         .manage(Arc::new(RwLock::new(
